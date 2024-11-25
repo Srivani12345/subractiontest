18b04e12ec01bcc44527e6eede60aa258ccc5c4f
srivani <mulukalasrivani@gmail.com>
2024-11-25 15:03:44 +0530

    subractionjavaprogram

---
+++ Subraction.java
@@ -0,0 +1,10 @@
+public class Subraction {
+	public static void main(String args[]) {
+		int num1=15;
+		int num2=5;
+
+		int difference=num1-num2;
+		System.out.println("The difference between"+num1+"and"+num2+"is:"+difference);
+	}
+}
+
